1a13c4f8ee026f0b43e2d3d72814fd01f8071e78
Andrey-Strekalov <71596678+Andrey-Strekalov@users.noreply.github.com>
2021-03-01 22:33:32 +0300

    Add files via upload

---
+++ add-and-delete-task.js
@@ -0,0 +1,37 @@
+function getID() {
+  let id;
+  if (tasksList.length === 0) {
+    id = 1;
+  } else {
+    let taskIds = tasksList.map((item) => item.id);
+    id = Math.max.apply(null, taskIds) + 1;
+  }
+  return String(id);
+}
+
+/* Добавляем задачу */
+
+function addNewTask() {
+  let input = document.getElementById("newTask");
+  tasksList.push({
+    id: getID(),
+    text: input.value,
+    completed: false,
+  });
+  renderTask(tasksList);
+  input.value = "";
+  checkFooter();
+  filterAll()
+}
+
+/* Удаляем задачу */
+
+function deleteTask(event) {
+  let id = event.target.parentNode.parentNode.id;
+  let newTasksList = tasksList.filter((task) => task.id !== id);
+  tasksList = newTasksList;
+  renderTask(tasksList);
+  checkFooter();
+  toggleDisplayBtn()
+  checkFilter();
+}
---
+++ footer.js
@@ -0,0 +1,87 @@
+/* Отображаем или скрываем кнопку clear completed */
+
+function toggleDisplayBtn() {
+  const btn = document.querySelector(".clear-completed");
+  const completedTasks = tasksList.filter((task) => task.completed);
+  btn.style.display = completedTasks.length > 0 ? "block" : "none";
+}
+
+toggleDisplayBtn();
+
+/* Удаляем выполненные задачи*/
+
+function clearCompleted() {
+  let newTaskList = tasksList.filter((task) => !task.completed);
+  tasksList = newTaskList;
+  renderTask(tasksList);
+  toggleDisplayBtn(); 
+  filterAll()
+  checkLocalStorage();
+  checkFooter();
+}
+
+/* Фильтры */
+
+/* все задачи */
+
+function filterAll() {
+  renderTask(tasksList);
+  const FilterValue = document.getElementById("all").getAttribute("href");
+  toggleFilterClass(FilterValue);
+}
+
+/* выполненные задачи */
+
+function filterCompleted() {
+  const newTasksList = tasksList.filter((task) => task.completed);
+  renderTask(newTasksList);
+  const FilterValue = document.getElementById("completed").getAttribute("href");
+  toggleFilterClass(FilterValue);
+}
+
+/* активные задачи */
+
+function filterActive() {
+  const newTasksList = tasksList.filter((task) => !task.completed);
+  renderTask(newTasksList);
+  const FilterValue = document.getElementById("active").getAttribute("href");
+  toggleFilterClass(FilterValue);
+}
+
+/* Отображаем или скрываем футер в зависимости от наличия задач */
+
+function checkFooter() {
+  const footer = document.querySelector("footer");
+  if (localStorage.getItem("task") !== null) {
+    footer.style.display = "block"
+  } else{
+    footer.style.display = "none"
+  }
+}
+
+/* Сохраняем выбранный фильтр после перезагрузки страницы */
+
+function checkFilter() {
+  const hash = window.location.hash;
+  if (hash == "#/") {
+    filterAll();
+  } else if (hash == "#/active") {
+    filterActive();
+  } else if (hash == "#/completed") {
+    filterCompleted();
+  }
+  toggleFilterClass(hash);
+}
+
+function toggleFilterClass(href) {
+  document.querySelectorAll(".FilterBtn").forEach((link) => {
+    if (link.getAttribute("href") == href) {
+      link.classList.add("selected");
+    } else {
+      link.classList.remove("selected");
+    }
+  });
+}
+
+checkFilter();
+checkFooter();
